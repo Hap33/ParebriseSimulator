f055d167dc90d0f2ffabb47c0d099645312f524e
MSI\Nico <lepilolodior7633@gmail.com>
2018-12-18 16:31:07 +0100

    Add SceneHUD

---
+++ Assets/Scripts/UiManagerInGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManagerInGame : MonoBehaviour
+{
+    public GameObject endPanel;
+    public GameObject hud;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickedButtonEndPanel()
+    {
+        hud.SetActive(false);
+        endPanel.SetActive(true);
+    }
+
+    public void OnClickedButtonMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OnClickedButtonNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
