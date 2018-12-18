4a9fde5950fb057bfae097a9ff7d883cd0f70acc
Hap33 <36961317+Hap33@users.noreply.github.com>
2018-12-18 16:07:33 +0100

    Push Menu vers Jeu

--- Assets/Scripts/UIManagerMenu.cs
+++ Assets/Scripts/UIManagerMenu.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManagerMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void ClickPlay()
+    {
+        SceneManager.LoadScene("Game");
+    }
 }
