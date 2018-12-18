59d85dbdb57a2b158b856785d93fbd0792885940
Hap33 <36961317+Hap33@users.noreply.github.com>
2018-12-18 16:13:29 +0100

    Fake Menu to loading to game

--- Assets/Scripts/UIManagerMenu.cs
+++ Assets/Scripts/UIManagerMenu.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIManagerMenu : MonoBehaviour {
+public class UIManagerMenu : MonoBehaviour
+{
 
-	public void ClickPlay()
+    public void ClickPlay()
     {
+        StartCoroutine(FakeTimer());
+    }
+
+    IEnumerator FakeTimer()
+    {
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("Game");
     }
 }
