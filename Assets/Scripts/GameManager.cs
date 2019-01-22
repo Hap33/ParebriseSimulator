dbb41ea2d9517b5df077ddabb8c83688172739ed
Hap33 <36961317+Hap33@users.noreply.github.com>
2019-01-22 14:24:36 +0100

    EnemySpawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    #region Singleton
+    public static GameManager Singleton;
+
+    private void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Singleton = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    public GameObject Player;
+    
+    void Start () {
+        Instantiate(Player);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (GameObject spawner in GameObject.FindGameObjectsWithTag("EnemySpawner"))
+            {
+                spawner.GetComponent<SpawnEnemy>().SpawnAnEnemy();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+            {
+                if (enemy.GetComponent<EnemyBehaviour>().IsInTrigger)
+                {
+                    Destroy(enemy);
+                    foreach (GameObject spawner in GameObject.FindGameObjectsWithTag("EnemySpawner"))
+                    {
+                        spawner.GetComponent<SpawnEnemy>().DestroyEnemy();
+                    }
+                }
+            }
+        }
+	}
+
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+    public GameObject Enemy;
+
+    private bool CanSpawnEnemy;
+
+    private void Start()
+    {
+        CanSpawnEnemy = true;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void SpawnAnEnemy()
+    {
+        if (CanSpawnEnemy)
+        {
+            Instantiate(Enemy, transform.position, transform.rotation);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy triggered");
+            CanSpawnEnemy = false;
+        }
+    }
+
+    public void DestroyEnemy()
+    {
+        CanSpawnEnemy = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            CanSpawnEnemy = true;
+        }
+    }
+}
